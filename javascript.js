64aeb9f18be57f28fc97da7a9c5f18b04f843a58
Your Name <you@example.com>
2019-07-31 16:30:24 -0500

    fixed my JS button i have been working on

--- javascript.js
+++ javascript.js
@@ -7,23 +7,14 @@ booksImg1.src = 'images/book.jpeg';
 
 
 
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
+
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.signbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+    let dropdowns = document.getElementsByClassName("dropdown-content");
   }
 }
 // const hColor = document.querySelectorAll('h2');
