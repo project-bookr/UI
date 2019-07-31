9f4e6ec686d8859bbfbcc21470f72fff95bde77e
Your Name <you@example.com>
2019-07-30 21:06:50 -0500

    trying to add DOM into the project and trying to find a good picture for the header so it does not look so blank

---
+++ javascript.js
@@ -0,0 +1,10 @@
+
+
+const bookImg = document.querySelector('.book1');
+bookImg.src = 'images/three-books.jpg';
+const booksImg1 = document.querySelector('.open-book');
+booksImg1.src = 'images/book.jpeg';
+
+
+// const hColor = document.querySelectorAll('h2');
+// hColor.style.color = 'pink';
